55c43fcdead974e48440d546a36643cbfc2eac48
🥩 Flora <floralph@amazon.com>
2020-02-21 14:54:46 -0800

    Update bell.py
    Replaced string QS1 ARN with AwsQuantumSimulatorArns.QS1 in bell pair example.

--- examples/bell.py
+++ examples/bell.py
@@ -1,10 +1,10 @@
 import boto3
-from braket.aws import AwsQuantumSimulator
+from braket.aws import AwsQuantumSimulator, AwsQuantumSimulatorArns
 from braket.circuits import Circuit
 
 aws_account_id = boto3.client("sts").get_caller_identity()["Account"]
 
-device = AwsQuantumSimulator("arn:aws:aqx:::quantum-simulator:aqx:qs1")
+device = AwsQuantumSimulator(AwsQuantumSimulatorArns.QS1)
 s3_folder = (f"braket-output-{aws_account_id}", "folder-name")
 
 # https://wikipedia.org/wiki/Bell_state
