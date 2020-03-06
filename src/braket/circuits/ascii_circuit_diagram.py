ab63ab5fd81d1432f178d6ec3c3a061adfe62145
Ava Wang <57644099+avawang1@users.noreply.github.com>
2020-03-06 15:35:15 -0800

    Fix depth and ascii diagrams for circuits with overlapping multi-qubi… (#44)

--- src/braket/circuits/ascii_circuit_diagram.py
+++ src/braket/circuits/ascii_circuit_diagram.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from typing import List
+from typing import List, Tuple
 
 from braket.circuits.circuit_diagram import CircuitDiagram
 from braket.circuits.gate import Gate
@@ -28,7 +28,7 @@ class AsciiCircuitDiagram(CircuitDiagram):
         Build an ASCII string circuit diagram.
 
         Args:
-            circuit (Circuit): Circuit to build a diagram of.
+            circuit (Circuit): Circuit for which to build a diagram.
 
         Returns:
             str: ASCII string circuit diagram.
@@ -69,22 +69,105 @@ class AsciiCircuitDiagram(CircuitDiagram):
         return "\n".join(lines)
 
     @staticmethod
+    def _ascii_moment_group_instructions(
+        instructions: List[Instruction],
+    ) -> List[Tuple[QubitSet, List[Instruction]]]:
+        """
+        Group instructions in a moment for ASCII diagram
+
+        Args:
+            instructions (List[Instruction]): list of instructions
+
+        Returns:
+            List[(QubitSet, List[Instruction])]: list of grouped instructions
+        """
+        groupings = []
+        for instr in instructions:
+            # Can only print Gate operators at the moment
+            if not isinstance(instr.operator, Gate):
+                continue
+
+            qubit_range = QubitSet(range(min(instr.target), max(instr.target) + 1))
+
+            found_grouping = False
+            for group in groupings:
+                qubits_added = group[0]
+                instr_group = group[1]
+                # Take into account overlapping multi-qubit gates
+                if not qubits_added.intersection(set(qubit_range)):
+                    instr_group.append(instr)
+                    qubits_added.update(qubit_range)
+                    found_grouping = True
+                    break
+
+            if not found_grouping:
+                groupings.append((qubit_range, [instr]))
+
+        return groupings
+
+    @staticmethod
     def _ascii_diagram_moment(
         time: int, circuit_qubits: QubitSet, instructions: List[Instruction]
     ) -> str:
         """
         Return an ASCII string diagram of the circuit at a particular moment in time.
 
+        Args:
+            time (int): time of moment
+            circuit_qubits (QubitSet): qubits in circuit
+            instructions (List[Instruction]): list of instructions
+
         Returns:
             str: An ASCII string diagram for the specified moment in time.
+        """
+
+        # Group instructions to separate out overlapping multi-qubit gates
+        groupings = AsciiCircuitDiagram._ascii_moment_group_instructions(instructions)
+
+        column_strs = [
+            AsciiCircuitDiagram._ascii_diagram_moment_column(circuit_qubits, grouping[1])
+            for grouping in groupings
+        ]
+
+        # Unite column strings
+        lines = column_strs[0].split("\n")
+        for column_str in column_strs[1:]:
+            for i, moment_line in enumerate(column_str.split("\n")):
+                lines[i] += moment_line
+
+        # Adjust for time width
+        time_width = len(str(time))
+        symbols_width = len(lines[0]) - 1
+        if symbols_width < time_width:
+            diff = time_width - symbols_width
+            for i in range(len(lines) - 1):
+                if lines[i].endswith("-"):
+                    lines[i] += "-" * diff
+                else:
+                    lines[i] += " "
+
+        first_line = "{:^{width}}|\n".format(str(time), width=len(lines[0]) - 1)
+
+        return first_line + "\n".join(lines)
+
+    @staticmethod
+    def _ascii_diagram_moment_column(
+        circuit_qubits: QubitSet, instructions: List[Instruction]
+    ) -> str:
+        """
+        Return an ASCII string diagram of the circuit at a particular moment in time for a column.
+
+        Args:
+            circuit_qubits (QubitSet): qubits in circuit
+            instructions (List[Instruction]): list of instructions
+
+        Returns:
+            str: An ASCII string diagram for the specified moment in time for a column.
         """
         symbols = {qubit: "-" for qubit in circuit_qubits}
         margins = {qubit: " " for qubit in circuit_qubits}
 
         for instr in instructions:
-            # Can only print Gate operators at the moment
-            if not isinstance(instr.operator, Gate):
-                continue
 
             qubits = circuit_qubits.intersection(
                 set(range(min(instr.target), max(instr.target) + 1))
@@ -100,13 +183,13 @@ class AsciiCircuitDiagram(CircuitDiagram):
                 else:
                     symbols[qubit] = "|"
 
-                # set the margin to be a connector if not on the first qubit
+                # Set the margin to be a connector if not on the first qubit
                 if qubit != min(instr.target):
                     margins[qubit] = "|"
 
-        symbols_width = max([len(symbol) for symbol in symbols.values()] + [len(str(time))])
+        symbols_width = max([len(symbol) for symbol in symbols.values()])
 
-        output = "{0:{width}}|\n".format(str(time), width=symbols_width)
+        output = ""
         for qubit in circuit_qubits:
             output += "{0:{width}}\n".format(margins[qubit], width=symbols_width + 1)
             output += "{0:{fill}{align}{width}}\n".format(
