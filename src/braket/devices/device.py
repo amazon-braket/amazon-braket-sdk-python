f7ca4a0905a3f01d2b9a47b621fc212436d0fa71
Ava Wang <avawang@amazon.com>
2020-08-12 17:03:01 -0700

    Minor change

--- src/braket/devices/device.py
+++ src/braket/devices/device.py
@@ -44,7 +44,7 @@ class Device(ABC):
             task_specification (Union[Circuit, Problem]):  Specification of a task
                 to run on device.
 
-            shots (int): The number of times to run the task on the device. Default is `none`.
+            shots (int): The number of times to run the task on the device. Default is `None`.
 
         Returns:
             QuantumTask: The QuantumTask tracking task execution on this device
