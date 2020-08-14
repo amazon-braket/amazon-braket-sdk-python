908c5fa0d306eff25ad138897c533743379d530f
ℂ𝓞𝔇𝚈 <caw@amazon.com>
2020-08-14 15:07:22 -0700

    documentation: Get Read the Docs working
    Fixed previously failing Read the Docs builds (see
    aws/amazon-braket-default-simulator-python#46)

--- doc/conf.py
+++ doc/conf.py
@@ -10,6 +10,7 @@ release = version
 copyright = "{}, Amazon.com".format(datetime.datetime.now().year)
 
 extensions = [
+    "sphinxcontrib.apidoc",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
@@ -28,3 +29,10 @@ html_theme = "sphinx_rtd_theme"
 htmlhelp_basename = "{}doc".format(project)
 
 napoleon_use_rtype = False
+
+apidoc_module_dir = "../src/braket"
+apidoc_output_dir = "_apidoc"
+apidoc_excluded_paths = ["../test"]
+apidoc_separate_modules = True
+apidoc_module_first = True
+apidoc_extra_args = ["-f", "--implicit-namespaces", "-H", "API Reference"]
