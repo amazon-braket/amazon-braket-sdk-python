966548a05ef4d26a1be55f898a626711154dece7
Aaron Berdy <berdy@amazon.com>
2021-01-13 15:35:44 -0800

    fix: get correct event loop for task results after running a batch over multiple threads

--- src/braket/aws/aws_quantum_task.py
+++ src/braket/aws/aws_quantum_task.py
@@ -274,7 +274,7 @@ class AwsQuantumTask(QuantumTask):
             return self._result
         try:
             async_result = self.async_result()
-            return asyncio.get_event_loop().run_until_complete(async_result)
+            return async_result.get_loop().run_until_complete(async_result)
         except asyncio.CancelledError:
             # Future was cancelled, return whatever is in self._result if anything
             self._logger.warning("Task future was cancelled")
