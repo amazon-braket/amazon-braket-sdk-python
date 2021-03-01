638a300d1d55119d1dd724d6e2879ef437deb5a9
Xiaosi Xu <66740305+xiaosi-xu@users.noreply.github.com>
2021-03-01 17:34:57 +0000

    Update gate_model_device_testing_utils.py

--- test/integ_tests/gate_model_device_testing_utils.py
+++ test/integ_tests/gate_model_device_testing_utils.py
@@ -447,6 +447,7 @@ def multithreaded_bell_pair_testing(device: Device, run_kwargs: Dict[str, Any]):
         assert np.allclose(result.measurement_probabilities["11"], 0.5, **tol)
         assert len(result.measurements) == shots
 
+
 def noisy_circuit_1qubit_noise_full_probability(device: Device, run_kwargs: Dict[str, Any]):
     shots = run_kwargs["shots"]
     tol = get_tol(shots)
@@ -464,9 +465,8 @@ def noisy_circuit_2qubit_noise_full_probability(device: Device, run_kwargs: Dict
     shots = run_kwargs["shots"]
     tol = get_tol(shots)
     K0 = np.eye(4) * np.sqrt(0.9)
-    K1 = np.kron(np.array([[0.0, 1.0], [1.0, 0.0]]), np.array([[0.0, 1.0], [1.0, 0.0]])) * np.sqrt(
-        0.1
-    )
+    K1 = np.kron(np.array([[0.0, 1.0], [1.0, 0.0]]),
+    np.array([[0.0, 1.0], [1.0, 0.0]])) * np.sqrt(0.1)
     circuit = Circuit().x(0).x(1).kraus((0, 1), [K0, K1]).probability()
     result = device.run(circuit, **run_kwargs).result()
     assert len(result.result_types) == 1
@@ -476,7 +476,7 @@ def noisy_circuit_2qubit_noise_full_probability(device: Device, run_kwargs: Dict
         **tol
     )
 
-    
+
 def batch_bell_pair_testing(device: AwsDevice, run_kwargs: Dict[str, Any]):
     shots = run_kwargs["shots"]
     tol = get_tol(shots)
