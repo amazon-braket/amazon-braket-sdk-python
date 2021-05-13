06185efc78cf33914b099867c8b4d305eaf3247c
Xiaosi Xu <66740305+xiaosi-xu@users.noreply.github.com>
2021-05-13 20:45:12 +0100

    add class MomentType

--- src/braket/circuits/moments.py
+++ src/braket/circuits/moments.py
@@ -13,6 +13,7 @@
 
 from __future__ import annotations
 
+from enum import Enum
 from typing import (
     Dict,
     ItemsView,
@@ -31,13 +32,30 @@ from braket.circuits.qubit import Qubit
 from braket.circuits.qubit_set import QubitSet
 
 
+class MomentType(str, Enum):
+    """
+    The type of moments.
+    GATE: a gate
+    NOISE: a noise channel added directly to the circuit
+    GATE_NOISE: a gate-based noise channel
+    INITIALIZATION_NOISE: a initialization noise channel
+    READOUT_NOISE: a readout noise channel
+    """
+
+    GATE = "gate"
+    NOISE = "noise"
+    GATE_NOISE = "gate_noise"
+    INITIALIZATION_NOISE = "initialization_noise"
+    READOUT_NOISE = "readout_noise"
+
+
 class MomentsKey(NamedTuple):
     """Key of the Moments mapping.
     Args:
      time: moment
      qubits: qubit set
-     moment_type: can be 'gate', 'noise', or 'gate_noise' which is associated with gates;
-                 and 'readout_noise' or 'initialization_noise'.
+     moment_type: can be GATE, NOISE, or GATE_NOISE which is associated with gates;
+                 and READOUT_NOISE or INITIALIZATION_NOISE.
      noise_index: the number of noise channels at the same moment. For gates, this is the
                  number of gate_noise channels associated with that gate. For all other noise
                  types, noise_index starts from 0; but for gate noise, it starts from 1.
@@ -45,7 +63,7 @@ class MomentsKey(NamedTuple):
 
     time: int
     qubits: QubitSet
-    moment_type: str
+    moment_type: MomentType
     noise_index: int
 
 
@@ -165,7 +183,9 @@ class Moments(Mapping[MomentsKey, Instruction]):
             for qubit in qubit_range:
                 self._max_times[qubit] = max(time, self._max_time_for_qubit(qubit))
 
-            self._moments[MomentsKey(time, instruction.target, "gate", noise_index)] = instruction
+            self._moments[
+                MomentsKey(time, instruction.target, MomentType.GATE, noise_index)
+            ] = instruction
             self._qubits.update(instruction.target)
             self._depth = max(self._depth, time + 1)
 
@@ -175,7 +195,7 @@ class Moments(Mapping[MomentsKey, Instruction]):
 
         qubit_range = instruction.target
         time = max(0, *[self._max_time_for_qubit(qubit) for qubit in qubit_range])
-        if input_type == "initialization_noise":
+        if input_type == MomentType.INITIALIZATION_NOISE:
             time = 0
 
         while MomentsKey(time, qubit_range, input_type, noise_index) in self._moments:
@@ -191,20 +211,20 @@ class Moments(Mapping[MomentsKey, Instruction]):
         1. Make the readout noise in the end
         2. Make the initialization noise at the beginning
         """
-        # key for 'noise', 'gate' and 'gate_noise'
+        # key for NOISE, GATE and GATE_NOISE
         key_noise = []
-        # key for 'initialization_noise'
+        # key for INITIALIZATION_NOISE
         key_initialization_noise = []
-        # key for 'readout_noise'
+        # key for READOUT_NOISE
         key_readout_noise = []
         moment_copy = OrderedDict()
         sorted_moment = OrderedDict()
 
         for key, instruction in self._moments.items():
             moment_copy[key] = instruction
-            if key.moment_type == "readout_noise":
+            if key.moment_type == MomentType.READOUT_NOISE:
                 key_readout_noise.append(key)
-            elif key.moment_type == "initialization_noise":
+            elif key.moment_type == MomentType.INITIALIZATION_NOISE:
                 key_initialization_noise.append(key)
             else:
                 key_noise.append(key)
@@ -218,7 +238,7 @@ class Moments(Mapping[MomentsKey, Instruction]):
 
         for key in key_readout_noise:
             sorted_moment[
-                MomentsKey(max_time, key.qubits, "readout_noise", key.noise_index)
+                MomentsKey(max_time, key.qubits, MomentType.READOUT_NOISE, key.noise_index)
             ] = moment_copy[key]
 
         self._moments = sorted_moment
