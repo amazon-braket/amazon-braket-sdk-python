9b9c059f5d1210f62241a33efe32fc6eacc6386c
Milan Krneta <krneta@amazon.com>
2021-06-22 09:40:18 -0700

    Modifying the doc for returning the CW client, and updating copyright range.

--- src/braket/aws/aws_session.py
+++ src/braket/aws/aws_session.py
@@ -137,12 +137,12 @@ class AwsSession(object):
         obj = s3.Object(s3_bucket, s3_object_key)
         return obj.get()["Body"].read().decode("utf-8")
 
-    def create_logs_client(self) -> Any:
+    def create_logs_client(self) -> "boto3.session.Session.client":
         """
         Create a CloudWatch Logs boto client.
 
         Returns:
-            Any: The CloudWatch Logs boto client.
+            'boto3.session.Session.client': The CloudWatch Logs boto client.
         """
         return self.boto_session.client("logs", config=self._config)
 
