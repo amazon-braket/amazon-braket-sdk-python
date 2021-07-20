72b16ee2bd57221e557cbfc6dd7eaae7bceb16df
Kshitij Chhabra <kshitijc@users.noreply.github.com>
2021-07-20 10:23:08 -0700

    feature: Add save/load_job_checkpoint and save_job_result functions (#10)

--- src/braket/jobs/__init__.py
+++ src/braket/jobs/__init__.py
@@ -11,7 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from braket.jobs import image_uris, persistence  # noqa: F401
 from braket.jobs.config import (  # noqa: F401
     CheckpointConfig,
     InstanceConfig,
