8ef5d3d81be3d1c7a2732d58e3e39f477d4a0619
Lin <licedric@amazon.com>
2021-09-15 23:00:00 -0700

    Update based on comments

--- examples/jobs/source_dir/qaoa_utils.py
+++ examples/jobs/source_dir/qaoa_utils.py
@@ -1,71 +1,131 @@
-# Import PennyLane / TF / Torch globally based on input.
-# This is necessary so we don't try to e.g. import tensorflow in the Torch container
-# Note "global" refers to this module only
-def import_interface(interface='autograd'):
-    if interface == 'autograd':
+from abc import ABC, abstractmethod
+
+
+class QAOAInterface(ABC):
+    @classmethod
+    @abstractmethod
+    # Initialize params to the appropriate format for the autodiff library.
+    def initialize_params(cls, np_array):
+        pass
+
+    @classmethod
+    @abstractmethod
+    # Returns the gradient descent optimizer to use, based on the ML framework.
+    def get_sgd_optimizer(cls, params, stepsize=0.1):
+        pass
+
+    @classmethod
+    @abstractmethod
+    # Convert params to base Numpy arrays.
+    def convert_params_to_numpy(cls, params):
+        pass
+
+    @classmethod
+    @abstractmethod
+    # Evaluate the cost function, then take a step.
+    def get_cost_and_step(cls, cost_function, params, optimizer):
+        pass
+
+    @staticmethod
+    # Get the appropriate interface to use based on the string input.
+    def get_interface(interface):
+        if interface == "autograd":
+            return AutogradInterface()
+        elif interface == "tf":
+            return TensorFlowInterface()
+        elif interface == "torch":
+            return PyTorchInterface()
+        else:
+            raise ValueError(f"Interface {interface} is invalid.")
+
+
+class AutogradInterface(QAOAInterface):
+    # Import only in __init__ so we don't import a library that doesn't exist in the container.
+    def __init__(self):
         global qml
-        qml = __import__('pennylane', globals(), locals()) 
+        qml = __import__("pennylane", globals(), locals())
         # Equiv to: import pennylane as qml
-    elif interface == 'tf':
-        global tf
-        tf = __import__('tensorflow', globals(), locals()) 
-        # Equiv to: import tensorflow as tf
-    elif interface == 'torch':
-        global torch
-        torch = __import__('torch', globals(), locals()) 
-        # Equiv to: import torch
 
-# Initialize params to the appropriate format for the autodiff library.
-def initialize_params(np_array, interface='autograd'):
-    if interface == 'autograd':
+    @classmethod
+    def initialize_params(cls, np_array):
         return np_array
-    elif interface == 'tf':
-        return tf.Variable(np_array, dtype=tf.float64)
-    elif interface == 'torch':
-        return torch.tensor(np_array, requires_grad=True)
-        
-# Returns the gradient descent optimizer to use, based on the ML framework.
-def get_sgd_optimizer(params, interface='autograd', stepsize=0.1):
-    if interface == 'autograd':
-        return qml.GradientDescentOptimizer(stepsize=stepsize)
-    elif interface == 'tf':
-        return tf.keras.optimizers.SGD(learning_rate=stepsize)
-    elif interface == 'torch':
-        return torch.optim.SGD([params], lr=stepsize)
-    
-# Convert params to base Numpy arrays.
-def convert_params_to_numpy(params, interface='autograd'):
-    if interface == 'autograd':
-        return params.numpy()
-    elif interface == 'tf':
-        return params.numpy()
-    elif interface == 'torch':
-        return params.detach().numpy()
 
-# Evaluate the cost function, then take a step.
-def get_cost_and_step(cost_function, params, optimizer, interface='autograd'):
-    if interface == 'autograd':
+    @classmethod
+    def get_sgd_optimizer(cls, params, stepsize=0.1):
+        return qml.GradientDescentOptimizer(stepsize=stepsize)
+
+    @classmethod
+    def convert_params_to_numpy(cls, params):
+        return params.numpy()
+
+    @classmethod
+    def get_cost_and_step(cls, cost_function, params, optimizer):
         params, cost_before = optimizer.step_and_cost(cost_function, params)
-    elif interface == 'tf':
+        return params, float(cost_before)
+
+
+class TensorFlowInterface(QAOAInterface):
+    # Import only in __init__ so we don't import a library that doesn't exist in the container.
+    def __init__(self):
+        global tf
+        tf = __import__("tensorflow", globals(), locals())
+        # Equiv to: import tensorflow as tf
+
+    @classmethod
+    def initialize_params(cls, np_array):
+        return tf.Variable(np_array, dtype=tf.float64)
+
+    @classmethod
+    def get_sgd_optimizer(cls, params, stepsize=0.1):
+        return tf.keras.optimizers.SGD(learning_rate=stepsize)
+
+    @classmethod
+    def convert_params_to_numpy(cls, params):
+        return params.numpy()
+
+    @classmethod
+    def get_cost_and_step(cls, cost_function, params, optimizer):
         def tf_cost():
             global _cached_cost_before
             _cached_cost_before = cost_function(params)
             return _cached_cost_before
+
         optimizer.minimize(tf_cost, params)
         cost_before = _cached_cost_before
-        
-#         Alternative:
-#         with tf.GradientTape() as tape:
-#             cost_before = cost_function(params)
 
-#         gradients = tape.gradient(cost_before, params)
-#         optimizer.apply_gradients(((gradients, params),))
-    elif interface == 'torch':
+        # Alternative:
+        # with tf.GradientTape() as tape:
+        #     cost_before = cost_function(params)
+        #
+        # gradients = tape.gradient(cost_before, params)
+        # optimizer.apply_gradients(((gradients, params),))
+
+        return params, float(cost_before)
+
+
+class PyTorchInterface(QAOAInterface):
+    # Import only in __init__ so we don't import a library that doesn't exist in the container.
+    def __init__(self):
+        global torch
+        torch = __import__("torch", globals(), locals())
+        # Equiv to: import torch
+
+    @classmethod
+    def initialize_params(cls, np_array):
+        return torch.tensor(np_array, requires_grad=True)
+
+    @classmethod
+    def get_sgd_optimizer(cls, params, stepsize=0.1):
+        return torch.optim.SGD([params], lr=stepsize)
+
+    @classmethod
+    def convert_params_to_numpy(cls, params):
+        return params.detach().numpy()
+
+    @classmethod
+    def get_cost_and_step(cls, cost_function, params, optimizer):
         optimizer.zero_grad()
         cost_before = cost_function(params)
         cost_before.backward()
         optimizer.step()
-    else:
-        pass
-
-    return params, float(cost_before)
+        return params, float(cost_before)
