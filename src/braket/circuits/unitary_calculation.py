aca61f235c83d1b5ee38e307dd6aa836ba8efb25
ℂ𝓞𝔇𝚈 <caw@amazon.com>
2022-03-23 16:37:08 -0700

    Descriptive comments

--- src/braket/circuits/unitary_calculation.py
+++ src/braket/circuits/unitary_calculation.py
@@ -21,18 +21,18 @@ from braket.circuits.qubit_set import QubitSet
 from braket.default_simulator.linalg_utils import multiply_matrix
 
 
-def calculate_unitary(instructions: Iterable[Instruction], used_qubits: QubitSet) -> np.ndarray:
+def calculate_unitary(instructions: Iterable[Instruction], qubits: QubitSet) -> np.ndarray:
     """
-    Returns the unitary matrix representation for all the `instructions` on qubits `used_qubits`.
+    Returns the unitary matrix representation for all the `instructions` on qubits `qubits`.
 
     Note:
         The performance of this method degrades with qubit count. It might be slow for
-        `len(used_qubits)` > 10.
+        `len(qubits)` > 10.
 
     Args:
         instructions (Iterable[Instruction]): The instructions for which the unitary matrix
             will be calculated.
-        used_qubits (QubitSet, optional): The actual qubits used by the instructions.
+        qubits (QubitSet, optional): The actual qubits used by the instructions.
 
     Returns:
         np.ndarray: A numpy array with shape (2^qubit_count, 2^qubit_count) representing the
@@ -42,17 +42,20 @@ def calculate_unitary(instructions: Iterable[Instruction], used_qubits: QubitSet
         TypeError: If `instructions` is not composed only of `Gate` instances,
             i.e. a circuit with `Noise` operators will raise this error.
     """
-    qubits = sorted(used_qubits)
-    qubit_count = len(qubits)
-    index_substitutions = {qubits[i]: i for i in range(qubit_count)}
-
+    qubits_sorted = sorted(qubits)
+    qubit_count = len(qubits_sorted)
+    # Map qubits to contiguous indices starting from 0
+    index_substitutions = {qubits_sorted[i]: i for i in range(qubit_count)}
     rank = 2**qubit_count
+    # Initialize identity unitary as type (rank, rank) tensor
     unitary = np.eye(rank).reshape([2] * 2 * qubit_count)
 
     for instruction in instructions:
         if not isinstance(instruction.operator, Gate):
             raise TypeError("Only Gate operators are supported to build the unitary")
-        targets = tuple(index_substitutions[qubit] for qubit in instruction.target)
-        unitary = multiply_matrix(unitary, instruction.operator.to_matrix(), targets)
+        unitary = multiply_matrix(
+            unitary, instruction.operator.to_matrix(),
+            tuple(index_substitutions[qubit] for qubit in instruction.target)
+        )
 
     return unitary.reshape(rank, rank)
