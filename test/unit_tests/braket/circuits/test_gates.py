883ec36b5607a7a9955da4083a06e0e249df6834
synthia2080 <cynthiasynthia2077@gmail.com>
2022-05-12 13:36:57 -0500

    commit on ry-gate, used linters to fix issues with Python Packages

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -307,7 +307,6 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
             "ry(0.17) $4;",
         ),
-
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
