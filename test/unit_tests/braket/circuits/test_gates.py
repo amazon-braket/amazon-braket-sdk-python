d98ef40e2d7a6e96970f6566f1a2e5f832ea5583
Synthia <93848130+synthia2080@users.noreply.github.com>
2022-05-17 11:29:05 -0500

    feature: support openQASM serialization of ry gate (#341)
    * feature: support openQASM serialization of ry gate

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -297,6 +297,18 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
             "rx(0.17) $4;",
         ),
+        (
+            Gate.Ry(angle=0.17),
+            [4],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
+            "ry(0.17) q[4];",
+        ),
+        (
+            Gate.Ry(angle=0.17),
+            [4],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
+            "ry(0.17) $4;",
+        ),
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
