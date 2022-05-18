46d8d800dead62bfcf22607d8f7eb9d32b583c11
Megha <57037661+mshenoy87@users.noreply.github.com>
2022-05-18 15:40:39 -0500

    feature: support openQASM serialization of y gate (#350)
    feature: support openQASM serialization of y gate

--- src/braket/circuits/gates.py
+++ src/braket/circuits/gates.py
@@ -188,6 +188,12 @@ class Y(Gate):
     def _to_jaqcd(self, target: QubitSet):
         return ir.Y.construct(target=target[0])
 
+    def _to_openqasm(
+        self, target: QubitSet, serialization_properties: OpenQASMSerializationProperties, **kwargs
+    ) -> str:
+        target_qubit = serialization_properties.format_target(int(target[0]))
+        return f"y {target_qubit};"
+
     def to_matrix(self) -> np.ndarray:
         return np.array([[0.0, -1.0j], [1.0j, 0.0]], dtype=complex)
 
