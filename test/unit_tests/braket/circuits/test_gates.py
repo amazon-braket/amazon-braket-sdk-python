6594e1c4b6d7f93bb71337ea2b904cd88fdc0de9
Synthia <93848130+synthia2080@users.noreply.github.com>
2022-05-20 13:04:00 -0500

    Update test_gates.py

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -369,10 +369,6 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
             "i $4;",
         ),
-<<<<<<< HEAD
-
-=======
->>>>>>> aws-feature/openqasm_generation
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
