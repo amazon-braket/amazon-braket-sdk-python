831dc858b97ef1da246c4714e7256b3d2154f3dc
mshenoy87 <Meghashenoy87@gmail.com>
2022-05-20 12:14:43 -0500

    feature: support openQASM serialization of zz gate

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -321,6 +321,18 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
             "ry(0.17) $4;",
         ),
+        (
+                Gate.ZZ(angle=0.17),
+                [4, 5],
+                OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
+                "zz(0.17) q[4], q[5];",
+        ),
+        (
+                Gate.ZZ(angle=0.17),
+                [4, 5],
+                OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
+                "zz(0.17) $4, $5;",
+        ),
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
