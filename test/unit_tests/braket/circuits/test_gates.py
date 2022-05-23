560a4cf57d56ef795b614d32c8220dbe045d791a
mshenoy87 <Meghashenoy87@gmail.com>
2022-05-22 20:43:03 -0500

    feature: support openQASM serialization of yy-gate

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -438,6 +438,18 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
             "cz q[0], q[1];",
         ),
+        (
+            Gate.YY(angle=0.17),
+            [4, 5],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
+            "yy(0.17) q[4], q[5];",
+        ),
+        (
+            Gate.YY(angle=0.17),
+            [4, 5],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
+            "yy(0.17) $4, $5;",
+        ),
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
