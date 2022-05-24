cf8132d7d456dfc902ddab092f7d901e28b8d833
Synthia <93848130+synthia2080@users.noreply.github.com>
2022-05-23 19:28:55 -0500

    Feature: Implemented openQASM serialization of iSwap-gate (#365)
    * feature: support openQASM serialization of ry gate
    
    * commit on ry-gate, used linters to fix issues with Python Packages
    
    * feature: support openQASM serialization of I-gate
    
    * feature: resolved some issues from improper capitalization in I gate and ran more tests to verify results
    
    * Update test_gates.py
    
    * Update test_gates.py
    
    * feature: Implemented openQASM serialization of iSwap-gate
    
    * fix: removed blank lines

--- test/unit_tests/braket/circuits/test_gates.py
+++ test/unit_tests/braket/circuits/test_gates.py
@@ -438,6 +438,18 @@ def test_ir_gate_level(testclass, subroutine_name, irclass, irsubclasses, kwargs
             OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
             "cz q[0], q[1];",
         ),
+        (
+            Gate.ISwap(),
+            [0, 1],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.PHYSICAL),
+            "iswap $0, $1;",
+        ),
+        (
+            Gate.ISwap(),
+            [0, 1],
+            OpenQASMSerializationProperties(qubit_reference_type=QubitReferenceType.VIRTUAL),
+            "iswap q[0], q[1];",
+        ),
     ],
 )
 def test_gate_to_ir_openqasm(gate, target, serialization_properties, expected_ir):
