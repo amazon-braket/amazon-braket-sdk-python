dafeb485de5b7e578d9de5dd3682ed50de60db7b
Kshitij Chhabra <chhabrk@amazon.com>
2022-11-01 16:11:51 -0700

    Add missing comma

--- .github/scripts/update_dependency.py
+++ .github/scripts/update_dependency.py
@@ -6,7 +6,7 @@ package = 'amazon-braket-sdk'
 path = Path.cwd().parent.resolve()
 print(f"Current path is {path}")
 for line in fileinput.input('setup.py', inplace=True):
-	replaced_line = line if package not in line else f'\"{package} @ file://{path}\"\n'
+	replaced_line = line if package not in line else f'\"{package} @ file://{path}\",\n'
 	print(replaced_line, end='')
 
 with open('setup.py', 'r') as f:
