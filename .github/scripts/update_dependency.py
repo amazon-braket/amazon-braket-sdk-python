894758caaaad22b85975921945cbeb640a20e7c8
Kshitij Chhabra <chhabrk@amazon.com>
2022-11-01 16:00:12 -0700

    Debug

--- .github/scripts/update_dependency.py
+++ .github/scripts/update_dependency.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 package = 'amazon-braket-sdk'
 path = Path.cwd().parent.resolve()
+print(f"Current path is {path}")
 for line in fileinput.input('setup.py', inplace=True):
 	replaced_line = line if package not in line else f'\"{package} @ file://{path}\"\n'
 	print(replaced_line, end='')
+
+with open('setup.py', 'r') as f:
+    print(f.read())
 
