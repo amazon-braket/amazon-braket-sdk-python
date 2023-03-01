9682d4c28ddf4d2184d0b58b6004ce1e6b417786
Stephen Face <shpface@amazon.com>
2023-02-28 18:05:16 -0800

    ignore long line warning for price url

--- src/braket/tracking/pricing.py
+++ src/braket/tracking/pricing.py
@@ -34,7 +34,7 @@ class Pricing:
         http = urllib3.PoolManager()
         price_url = os.environ.get(
             "BRAKET_PRICE_OFFERS_URL",
-            "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonBraket/current/index.csv",
+            "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonBraket/current/index.csv",  # noqa: E501
         )
         response = http.request(
             "GET",
