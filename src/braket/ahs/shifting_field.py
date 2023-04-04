1ff8a7295d3894a37aee1339a14dbab609d78fd1
Yaroslav Kharkov <ykharkov@amazon.com>
2023-04-04 15:20:43 -0400

    formatting

--- src/braket/ahs/shifting_field.py
+++ src/braket/ahs/shifting_field.py
@@ -90,11 +90,11 @@ class ShiftingField(Hamiltonian):
         Args:
             other (ShiftingField): The second shifting field to be stitched
             boundary (str): {"mean", "left", "right"}. Boundary point handler.
-            Possible options are
-                * "mean" - take the average of the boundary value points of the first
-                and the second time series.
-                * "left" - use the last value from the left time series as the boundary point.
-                * "right" - use the first value from the right time series as the boundary point.
+                Possible options are
+                    * "mean" - take the average of the boundary value points of the first
+                    and the second time series.
+                    * "left" - use the last value from the left time series as the boundary point.
+                    * "right" - use the first value from the right time series as the boundary point.
 
         Returns:
             ShiftingField: The stitched ShiftingField object.
