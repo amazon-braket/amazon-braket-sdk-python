3bfcdcc0b8ca35135978438ec3fa5265e4b8d2fe
Yaroslav Kharkov <ykharkov@amazon.com>
2023-04-04 13:15:26 -0400

    Formatting

--- src/braket/timings/time_series.py
+++ src/braket/timings/time_series.py
@@ -157,11 +157,12 @@ class TimeSeries:
         Args:
             other (TimeSeries): The second time series to be concatenated
             boundary (str): {"mean", "left", "right"}. Boundary point handler.
-            Possible options are
-                * "mean" - take the average of the boundary value points of the first
-                and the second time series.
-                * "left" - use the last value from the left time series as the boundary point.
-                * "right" - use the first value from the right time series as the boundary point.
+                Possible options are
+                    * "mean" - take the average of the boundary value points of the first
+                    and the second time series.
+                    * "left" - use the last value from the left time series as the boundary point.
+                    * "right" - use the first value from the right time series as the boundary 
+                    point.
         Returns:
             TimeSeries: The stitched time series.
         """
