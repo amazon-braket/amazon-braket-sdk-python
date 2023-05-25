7f31fce3f74fe37b1108d8783469b68785a6417b
Aaron Berdy <berdy@amazon.com>
2023-05-25 09:51:25 -0700

    feat: ascii printing for power modifier

--- src/braket/circuits/ascii_circuit_diagram.py
+++ src/braket/circuits/ascii_circuit_diagram.py
@@ -270,7 +270,19 @@ class AsciiCircuitDiagram(CircuitDiagram):
                     item_qubit_index = [
                         index for index, q in enumerate(target_qubits) if q == qubit
                     ][0]
-                    symbols[qubit] = ascii_symbols[item_qubit_index]
+                    power_string = (
+                        f"^{power}"
+                        if (
+                            (power := getattr(item, "power", 1)) != 1
+                            and ascii_symbols[item_qubit_index] != "C"
+                        )
+                        else ""
+                    )
+                    symbols[qubit] = (
+                        f"({ascii_symbols[item_qubit_index]}{power_string})"
+                        if power_string
+                        else ascii_symbols[item_qubit_index]
+                    )
                 elif qubit in control_qubits:
                     symbols[qubit] = "C"
                 else:
