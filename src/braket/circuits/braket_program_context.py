6426882a0797ae885fd195a6996ee61a735c6cf9
Sai Prakash Ch <ch.saiprakash22@gmail.com>
2023-07-10 11:14:26 -0700

    lint

--- src/braket/circuits/braket_program_context.py
+++ src/braket/circuits/braket_program_context.py
@@ -20,11 +20,9 @@ from braket.circuits.gates import Unitary
 from braket.circuits.noises import Kraus
 from braket.circuits.translations import (
     BRAKET_GATES,
-    braket_noise_gate_to_instruction,
     braket_result_to_result_type,
     one_prob_noise_map,
 )
-from braket.default_simulator import KrausOperation
 from braket.default_simulator.openqasm.program_context import AbstractProgramContext
 from braket.ir.jaqcd.program_v1 import Results
 
@@ -44,14 +42,6 @@ class BraketProgramContext(AbstractProgramContext):
         return self._circuit
 
     def is_builtin_gate(self, name: str) -> bool:
-        """Whether the gate is currently in scope as a built-in Braket gate.
-
-        Args:
-            name (str): name of the built-in Braket gate
-
-        Returns:
-            bool: return TRUE if it is a built-in gate else FALSE.
-        """
         user_defined_gate = self.is_user_defined_gate(name)
         return name in BRAKET_GATES and not user_defined_gate
 
@@ -61,18 +51,6 @@ class BraketProgramContext(AbstractProgramContext):
     def add_gate_instruction(
         self, gate_name: str, target: Tuple[int], *params, ctrl_modifiers: List[int], power: float
     ) -> None:
-        """Add Braket gate to the circuit.
-
-        Args:
-            gate_name (str): name of the built-in Braket gate.
-            target (Tuple[int]): control_qubits + target_qubits.
-            ctrl_modifiers (List[int]): Quantum state on which to control the
-                operation. Must be a binary sequence of same length as number of qubits in
-                `control-qubits` in target. For example "0101", [0, 1, 0, 1], 5 all represent
-                controlling on qubits 0 and 2 being in the \\|0⟩ state and qubits 1 and 3 being
-                in the \\|1⟩ state.
-            power(float): Integer or fractional power to raise the gate to.
-        """
         target_qubits = target[len(ctrl_modifiers) :]
         control_qubits = target[: len(ctrl_modifiers)]
         instruction = Instruction(
@@ -89,12 +67,6 @@ class BraketProgramContext(AbstractProgramContext):
         unitary: np.ndarray,
         target: Tuple[int],
     ) -> None:
-        """Add a custom Unitary instruction to the circuit
-
-        Args:
-            unitary (np.ndarray): unitary matrix
-            target (Tuple[int]): control_qubits + target_qubits
-        """
         instruction = Instruction(Unitary(unitary), target)
         self._circuit.add_instruction(instruction)
 
@@ -109,5 +81,4 @@ class BraketProgramContext(AbstractProgramContext):
         self._circuit.add_instruction(instruction)
 
     def add_result(self, result: Results) -> None:
-        """Add a result type to the circuit"""
         self._circuit.add_result_type(braket_result_to_result_type(result))
