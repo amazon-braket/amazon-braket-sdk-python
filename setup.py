ef7d115e9895f19c1859708e94f35159170e004e
Abe Coull <85974725+math411@users.noreply.github.com>
2023-08-16 09:40:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_namespace_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
     install_requires=[
-        "amazon-braket-schemas>=1.19.0",
+        "amazon-braket-schemas>=1.19.1",
         "amazon-braket-default-simulator>=1.19.1",
         "oqpy~=0.2.1",
         "setuptools",
