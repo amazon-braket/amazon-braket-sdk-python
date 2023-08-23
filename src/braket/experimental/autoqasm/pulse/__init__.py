3bc85e812543324ebf942302ffe23d54fc4f461a
Tim <yitchen@amazon.com>
2023-08-23 13:05:51 -0400

    fix: docstring typos

--- src/braket/experimental/autoqasm/pulse/__init__.py
+++ src/braket/experimental/autoqasm/pulse/__init__.py
@@ -11,13 +11,13 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-"""Pulse programming is a level of programming that controls qubits or frame in a lower level than
+"""Pulse programming is a level of programming that controls qubits or frames at a lower level than
 gates, such as playing a waveform or shifting the frequency of a frame. This module includes
 instructions for pulse programming. A program can have only pulse instructions, or mixed usage of
-pulse and gate instructions, if the device supports it. Pulse programming can also used to define
+pulse and gate instructions, if the device supports it. Pulse programming can also be used to define
 the calibration of gates in a program.
 
-Example of a program uses only pulse instructions:
+Example of a program that uses only pulse instructions:
 
 .. code-block:: python
 
