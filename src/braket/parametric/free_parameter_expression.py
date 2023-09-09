f4d89f8b1d65bc11276ef011de37c436dc9a37d9
Jean-Christophe Jaskula <jjaskula@amazon.com>
2023-09-08 20:15:06 -0400

    move to_ast to FreeParameterExprsesion

--- src/braket/parametric/free_parameter_expression.py
+++ src/braket/parametric/free_parameter_expression.py
@@ -41,6 +41,7 @@ class FreeParameterExpression:
 
         Args:
             expression (Union[FreeParameterExpression, Number, Expr, str]): The expression to use.
+            _type (Optional[ClassicalType]): type of the expression
 
         Raises:
             NotImplementedError: Raised if the expression is not of type
@@ -57,6 +58,7 @@ class FreeParameterExpression:
             ast.Pow: self.__pow__,
             ast.USub: self.__neg__,
         }
+        self._type = _type if _type is not None else FloatType()
         if isinstance(expression, FreeParameterExpression):
             self._expression = expression.expression
         elif isinstance(expression, (Number, sympy.Expr)):
