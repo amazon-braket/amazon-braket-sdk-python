376d694d64ee191c98faa4b0c4ed0d391f1ee468
Viraj Chaudhari <72896239+virajvchaudhari@users.noreply.github.com>
2023-09-25 17:31:00 -0700

    update github script

--- .github/scripts/update_dependency.py
+++ .github/scripts/update_dependency.py
@@ -22,7 +22,7 @@ from pathlib import Path
 # dependency, we can do this for the ones we own to make sure that when the sdk updates
 # its dependencies, these upstream github repos will not be impacted.
 
-package = "amazon-braket-sdk-staging"  # TODO: remove this before merging
+package = "amazon-braket-sdk"  # TODO: remove this before merging
 path = Path.cwd().parent.resolve()
 
 for line in fileinput.input("setup.py", inplace=True):
