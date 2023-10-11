45d61b2a9f03ba959d91a44409e1d31a006914fa
Aaron Berdy <berdy@amazon.com>
2023-10-10 18:04:16 -0700

    Update src/braket/jobs/data_persistence.py
    Co-authored-by: Cody Wang <speller26@gmail.com>

--- src/braket/jobs/data_persistence.py
+++ src/braket/jobs/data_persistence.py
@@ -134,7 +134,7 @@ def load_job_result(filename: str | Path = None) -> dict[str, Any]:
             must be in the format used by `save_job_result`.
 
     Returns:
-         dict[str, Any]: Job result data of current job
+        dict[str, Any]: Job result data of current job
     """
     persisted_data = _load_persisted_data(filename)
     deserialized_data = deserialize_values(persisted_data.dataDictionary, persisted_data.dataFormat)
