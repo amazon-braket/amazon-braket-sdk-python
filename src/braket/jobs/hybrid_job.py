84706baeb929786987d25e616cadbfc795a7f4a3
Aaron Berdy <berdy@amazon.com>
2023-10-12 11:21:10 -0700

    Revert "feat: add dec- prefix to decorator jobs (#739)"
    This reverts commit 0344d62220789910afe2596c0258d107553a5511.

--- src/braket/jobs/hybrid_job.py
+++ src/braket/jobs/hybrid_job.py
@@ -174,8 +174,7 @@ def hybrid_job(
                         f"{temp_dir}.{entry_point_file_path.stem}:{entry_point.__name__}"
                     ),
                     "wait_until_complete": wait_until_complete,
-                    "job_name": job_name
-                    or _generate_default_job_name(func=entry_point, decorator=True),
+                    "job_name": job_name or _generate_default_job_name(func=entry_point),
                     "hyperparameters": _log_hyperparameters(entry_point, args, kwargs),
                     "logger": logger,
                 }
