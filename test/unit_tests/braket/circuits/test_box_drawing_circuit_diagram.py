4f363a3377fc0ca9b23008d693a95df3aebbb15c
Jean-Christophe Jaskula <jjaskula@amazon.com>
2024-01-19 13:01:50 -0500

    remove commented tests

--- test/unit_tests/braket/circuits/test_box_drawing_circuit_diagram.py
+++ test/unit_tests/braket/circuits/test_box_drawing_circuit_diagram.py
@@ -959,30 +959,6 @@ def test_hamiltonian():
     _assert_correct_diagram(circ, expected)
 
 
-# def test_multi_qubit_Foo():
-#     class Foo(Gate):
-#         def __init__(self):
-#             super().__init__(qubit_count=2, ascii_symbols=["FOO"])
-
-#     circ = Circuit()
-#     circ.add_instruction(Instruction(Foo(), (0, 1)))
-#     circ.add_instruction(Instruction(Foo(), (0, 2)))
-#     expected = (
-#         "T  : │   0   │   1   │",
-#         "      ┌─────┐ ┌─────┐ ",
-#         "q0 : ─┤     ├─┤     ├─",
-#         "      │ FOO │ │     │ ",
-#         "      │     │ │     │ ",
-#         "q1 : ─┤     ├─│ FOO │─",
-#         "      └─────┘ │     │ ",
-#         "              │     │ ",
-#         "q2 : ─────────┤     ├─",
-#         "              └─────┘ ",
-#         "T  : │   0   │   1   │",
-#     )
-#     _assert_correct_diagram(circ, expected)
-
-
 def test_power():
     class Foo(Gate):
         def __init__(self):
