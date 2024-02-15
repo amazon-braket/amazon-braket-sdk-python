6ca84a8fa222e5cf7537983ae8c1af3a543b7468
Milan Krneta <krneta@amazon.com>
2024-02-15 13:03:04 -0800

    mistakenly removed lucky from testing

--- test/integ_tests/test_cost_tracking.py
+++ test/integ_tests/test_cost_tracking.py
@@ -28,7 +28,7 @@ from braket.tracking.tracker import MIN_SIMULATOR_DURATION
     "qpu",
     [
         "arn:aws:braket:us-east-1::device/qpu/ionq/Harmony",
-        "",
+        "arn:aws:braket:eu-west-2::device/qpu/oqc/Lucy",
         "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-3",
     ],
 )
