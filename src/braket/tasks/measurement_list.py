bd0f7c2b3fd5d4bb20d2695ff607a6dd6cc79426
Ashlyn Hanson <65787294+ashlhans@users.noreply.github.com>
2024-02-23 13:48:07 -0800

    Fix formatting

--- src/braket/tasks/measurement_list.py
+++ src/braket/tasks/measurement_list.py
@@ -11,7 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from collections import Counter
 from typing import Optional
 
 import numpy as np
