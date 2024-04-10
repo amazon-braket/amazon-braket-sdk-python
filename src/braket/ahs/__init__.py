2484f78e7f1c1b0b3c24daab83d2b98fc00307d8
Coull <accoull@amazon.com>
2024-04-10 15:18:29 -0700

    export ordering

--- src/braket/ahs/__init__.py
+++ src/braket/ahs/__init__.py
@@ -17,6 +17,6 @@ from braket.ahs.discretization_types import DiscretizationProperties  # noqa: F4
 from braket.ahs.driving_field import DrivingField  # noqa: F401
 from braket.ahs.field import Field  # noqa: F401
 from braket.ahs.hamiltonian import Hamiltonian  # noqa: F401
+from braket.ahs.local_detuning import LocalDetuning  # noqa: F401
 from braket.ahs.pattern import Pattern  # noqa: F401
-from braket.ahs.shifting_field import LocalDetuning  # noqa: F401
 from braket.ahs.shifting_field import ShiftingField  # noqa: F401
