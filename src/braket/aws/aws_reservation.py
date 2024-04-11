0ca5c2faed4ff4fb7c8a7b15ca0283ae15ae9504
Matt Beach <drbeach@amazon.com>
2024-04-11 09:44:38 -0400

    feature: context manager for reservation arns

---
+++ src/braket/aws/aws_reservation.py
@@ -0,0 +1,70 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from collections.abc import Callable
+from contextlib import contextmanager
+
+from braket.aws.aws_device import AwsDevice, AwsDeviceType
+from braket.devices.device import Device
+from braket.devices.local_simulator import LocalSimulator
+
+
+@contextmanager
+def Reservation(device: Device, reservation_arn: str | None = None) -> Device | None:
+    """
+    A context manager that temporarily modifies the `run` method of a `device` object
+    to include a specified `reservation_arn` in all calls within the context. This is useful
+    for ensuring that all quantum task run within the context are associated with a given
+    reservation ARN (Amazon Resource Name).
+
+    Args:
+        device (Device): The Braket device for which you have a reservation ARN.
+        reservation_arn (str | None): The Braket Direct reservation ARN to be applied to all
+            quantum tasks run within the context. Defaults to None.
+
+    Yields:
+        Device | None: The device object with a potentially modified `run` method. If the device is
+        a local simulator or the reservation ARN is None, the original device is yielded without
+        modification.
+
+    Examples:
+        >>> with Reservation(device, reservation_arn="<my_reservation_arn>"):
+        ...     task1 = device.run(circuit, shots)
+        ...     task2 = device.run(circuit, shots)
+    """
+
+    if not isinstance(device, Device):
+        raise ValueError("The provided device is not a valid Braket device.")
+
+    # Local simulators do not accept reservation ARNs
+    if isinstance(device, LocalSimulator):
+        yield device
+
+    elif isinstance(device, AwsDevice):
+        # on-demand simulators do not accept reservation ARNs
+        if device.type == AwsDeviceType.SIMULATOR:
+            yield device
+
+        elif device.type == AwsDeviceType.QPU:
+            original_run = device.run
+
+            def _run_with_reservation(*args, **kwargs) -> Callable:
+                kwargs["reservation_arn"] = reservation_arn
+                return original_run(*args, **kwargs)
+
+            device.run = _run_with_reservation
+
+            try:
+                yield device
+            finally:
+                device.run = original_run
---
+++ test/unit_tests/braket/aws/test_aws_reservation.py
@@ -0,0 +1,60 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from unittest.mock import Mock
+
+import pytest
+
+from braket.aws.aws_device import AwsDevice, AwsDeviceType
+from braket.aws.aws_reservation import Reservation
+from braket.devices.local_simulator import LocalSimulator
+
+
+def test_non_braket_device():
+    non_device = Mock()
+    with pytest.raises(ValueError):
+        with Reservation(non_device):
+            pass
+
+
+def test_local_simulator():
+    local_simulator = Mock(spec=LocalSimulator)
+    local_simulator.run = Mock()
+
+    with Reservation(local_simulator, reservation_arn="arn:test"):
+        local_simulator.run("circuit", 100)
+
+    local_simulator.run.assert_called_once_with("circuit", 100)
+
+
+def test_aws_simulator():
+    simulator_device = Mock(spec=AwsDevice)
+    simulator_device.type = AwsDeviceType.SIMULATOR
+    simulator_device.run = Mock()
+
+    with Reservation(simulator_device, reservation_arn="arn:test"):
+        simulator_device.run("circuit", 100)
+
+    simulator_device.run.assert_called_once_with("circuit", 100)
+
+
+@pytest.mark.parametrize("reservation_arn", [None, "arn:test"])
+def test_qpu_device(reservation_arn):
+    qpu_device = Mock(spec=AwsDevice)
+    qpu_device.type = AwsDeviceType.QPU
+    qpu_device.run = Mock()
+
+    with Reservation(qpu_device, reservation_arn=reservation_arn):
+        qpu_device.run("circuit", 100)
+
+    qpu_device.run.assert_called_once_with("circuit", 100, reservation_arn=reservation_arn)
