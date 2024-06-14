edef55d29a1ff6831efc8187079decc1abe673ea
Nagji <ltnln@amazon.com>
2024-06-14 12:04:00 -0700

    fix: Include EmulatorCriterion in __init__.py for emulator_passes module

--- src/braket/emulators/emulator_passes/__init__.py
+++ src/braket/emulators/emulator_passes/__init__.py
@@ -1,5 +1,6 @@
 from braket.emulators.emulator_passes.emulator_pass import EmulatorPass, ProgramType
 from braket.emulators.emulator_passes.criteria import (
+    EmulatorCriterion,
     ConnectivityCriterion,
     SupportedGateCriterion,
     NativeGateCriterion,
