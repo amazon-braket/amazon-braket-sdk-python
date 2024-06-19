0ecd0fad958e17f74ae661adcc4e41c209f5dbd3
Nagji <ltnln@amazon.com>
2024-06-18 22:46:20 -0700

    fix: Add tket_to_qasm3 to pytket_translator module __init__.py

--- src/braket/emulators/pytket_translator/__init__.py
+++ src/braket/emulators/pytket_translator/__init__.py
@@ -4,3 +4,4 @@ from braket.emulators.pytket_translator.translations import (
     QASM_TO_PYTKET
 )
 from braket.emulators.pytket_translator.pytket_program_context import PytketProgramContext
+from braket.emulators.pytket_translator.qasm3_gen import tket_to_qasm3
