e2c4b558420e52b25641d5937f559895145e196b
Nagji <ltnln@amazon.com>
2024-06-24 13:44:38 -0700

    fix: Update add_instruction to update circuit _measurement_targets at the instruction level instead of only if add_measure is called.

--- src/braket/circuits/circuit.py
+++ src/braket/circuits/circuit.py
@@ -503,7 +503,14 @@ class Circuit:
 
         # Check if there is a measure instruction on the circuit
         self._check_if_qubit_measured(instruction, target, target_mapping)
-
+        
+        #Update measure targets if instruction is a measurement
+        if isinstance(instruction.operator, Measure):
+            if self._measure_targets:
+                self._measure_targets.append(target or instruction.target[0])
+            else:
+                self._measure_targets = [target or instruction.target[0]]
+                
         if not target_mapping and not target:
             # Nothing has been supplied, add instruction
             instructions_to_add = [instruction]
@@ -710,10 +717,7 @@ class Circuit:
                     target=target,
                 )
             )
-            if self._measure_targets:
-                self._measure_targets.append(target)
-            else:
-                self._measure_targets = [target]
+
 
     def measure(self, target_qubits: QubitSetInput) -> Circuit:
         """
