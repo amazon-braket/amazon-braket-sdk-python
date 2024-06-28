cdc66d6475971c0d1cfb8b32674acb3c2cae4993
Cody Wang <speller26@gmail.com>
2024-06-28 13:13:30 -0700

    Update test_device_creation.py

--- test/integ_tests/test_device_creation.py
+++ test/integ_tests/test_device_creation.py
@@ -23,9 +23,7 @@ SIMULATOR_ARN = "arn:aws:braket:::device/quantum-simulator/amazon/sv1"
 PULSE_ARN = "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-3"
 
 
-@pytest.mark.parametrize(
-    "arn", [(RIGETTI_ARN), (IONQ_ARN), (SIMULATOR_ARN), (PULSE_ARN)]
-)
+@pytest.mark.parametrize("arn", [(RIGETTI_ARN), (IONQ_ARN), (SIMULATOR_ARN), (PULSE_ARN)])
 def test_device_creation(arn, created_braket_devices):
     device = created_braket_devices[arn]
     assert device.arn == arn
