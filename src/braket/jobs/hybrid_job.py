df98a41fd88be216d67a87367345f6ec2c7d5a21
Tim <yitchen@amazon.com>
2025-02-10 12:46:51 -0500

    fix: linters

--- src/braket/jobs/hybrid_job.py
+++ src/braket/jobs/hybrid_job.py
@@ -262,7 +262,7 @@ def persist_inner_function_source(entry_point: callable) -> None:
     """
     inner_source_mapping = _get_inner_function_source(entry_point.__code__)
 
-    if len(inner_source_mapping)==0:
+    if len(inner_source_mapping) == 0:
         yield {}
     else:
         with tempfile.TemporaryDirectory(dir="", prefix="decorator_job_inner_source_") as temp_dir:
