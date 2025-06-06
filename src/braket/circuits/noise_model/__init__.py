4ea7d7d8a272896bfd44e1d608bd6ea92ec5fb30
Sai Nandan Morapakula <sainandanm2002@gmail.com>
2025-06-06 00:13:11 -0400

    Update __init__.py

--- src/braket/circuits/noise_model/__init__.py
+++ src/braket/circuits/noise_model/__init__.py
@@ -15,26 +15,28 @@ from braket.circuits.noise_model.circuit_instruction_criteria import (
     CircuitInstructionCriteria,  # noqa: F401
 )
 from braket.circuits.noise_model.criteria import (
-    Criteria,  # noqa: F401
-    CriteriaKey,  # noqa: F401
-    CriteriaKeyResult,  # noqa: F401
+    Criteria,
+    CriteriaKey,
+    CriteriaKeyResult,
 )
-from braket.circuits.noise_model.gate_criteria import GateCriteria  # noqa: F401
-from braket.circuits.noise_model.initialization_criteria import InitializationCriteria  # noqa: F401
-from braket.circuits.noise_model.measure_criteria import MeasureCriteria  # noqa: F401
+from braket.circuits.noise_model.gate_criteria import GateCriteria
+from braket.circuits.noise_model.initialization_criteria import InitializationCriteria
+from braket.circuits.noise_model.measure_criteria import MeasureCriteria
 from braket.circuits.noise_model.noise_model import (
-    NoiseModel,  # noqa: F401
-    NoiseModelInstruction,  # noqa: F401
+    NoiseModel,
+    NoiseModelInstruction,
 )
-from braket.circuits.noise_model.observable_criteria import ObservableCriteria  # noqa: F401
+from braket.circuits.noise_model.observable_criteria import ObservableCriteria
 from braket.circuits.noise_model.qubit_initialization_criteria import (
-    QubitInitializationCriteria,  # noqa: F401
+    QubitInitializationCriteria,
 )
-from braket.circuits.noise_model.result_type_criteria import ResultTypeCriteria  # noqa: F401
-from braket.circuits.noise_model.unitary_gate_criteria import UnitaryGateCriteria  # noqa: F401
+from braket.circuits.noise_model.result_type_criteria import ResultTypeCriteria
+from braket.circuits.noise_model.unitary_gate_criteria import UnitaryGateCriteria
 
 __all__ = [
+    "Criteria",
     "CriteriaKey",
+    "CriteriaKeyResult",
     "GateCriteria",
     "InitializationCriteria",
     "MeasureCriteria",
