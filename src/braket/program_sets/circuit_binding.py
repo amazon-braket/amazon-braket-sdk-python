8437fbbafdb32e2caeae4d7f78c16457a0946678
Cody Wang <speller26@gmail.com>
2026-01-15 19:42:48 -0800

    Use changes in `bind_observables_to_inputs`

--- src/braket/program_sets/circuit_binding.py
+++ src/braket/program_sets/circuit_binding.py
@@ -75,11 +75,6 @@ class CircuitBinding:
         self._circuit = circuit
         self._input_sets = ParameterSets(input_sets) if input_sets else None
         self._observables = CircuitBinding._get_observables(observables)
-        # with_euler_angles validates that the observable has valid Euler angle gates
-        self._circuit_with_euler_angles = (
-            self._circuit.with_euler_angles(self._observables) if observables else None
-        )
-        self._euler_angles = self._get_euler_angles()
 
     @staticmethod
     def _get_observables(
@@ -99,47 +94,6 @@ class CircuitBinding:
                 pauli = PauliString(o)
                 obs.append(pauli.phase * pauli.to_unsigned_observable(include_trivial=True))
         return obs
-
-    def _get_euler_angles(self) -> dict[str, float] | None:
-        observables = self._observables
-        if not observables:
-            return None
-        return (
-            self._get_euler_angles_sum(observables)
-            if isinstance(observables, Sum)
-            else self._get_euler_angles_list(observables)
-        )
-
-    def _get_euler_angles_sum(self, observables: Sum) -> dict[str, float]:
-        euler_angles = defaultdict(list)
-        summands = observables.summands
-        if not observables.targets:
-            targets = self._circuit.qubits
-            for obs in summands:
-                for param, angle in obs.get_euler_angles(targets).items():
-                    euler_angles[param].append(angle)
-            return euler_angles
-        targets = {q for obs in summands for q in obs.targets}
-        for obs in summands:
-            obs_euler_angles = obs.euler_angles
-            for q in targets:
-                for param in euler_angle_parameter_names(q):
-                    euler_angles[param].append(obs_euler_angles.get(param, 0))
-        return euler_angles
-
-    def _get_euler_angles_list(self, observables: Sequence[Observable]) -> dict[str, float]:
-        euler_angles = defaultdict(list)
-        targets = {q for obs in observables for q in (obs.targets or self._circuit.qubits)}
-        for obs in observables:
-            if not obs.targets:
-                for param, angle in obs.get_euler_angles(targets).items():
-                    euler_angles[param].append(angle)
-            else:
-                obs_euler_angles = obs.euler_angles
-                for q in targets:
-                    for param in euler_angle_parameter_names(q):
-                        euler_angles[param].append(obs_euler_angles.get(param, 0))
-        return euler_angles
 
     @property
     def circuit(self) -> Circuit:
@@ -186,16 +140,58 @@ class CircuitBinding:
                 ).source,
                 inputs=self._input_sets.as_dict() if self._input_sets else None,
             )
+        # with_euler_angles validates that the observable has valid Euler angle gates
+        circuit_with_euler_angles = self._circuit.with_euler_angles(self._observables)
+        euler_angles = self._get_euler_angles()
         return Program(
-            source=self._circuit_with_euler_angles.to_ir(
+            source=circuit_with_euler_angles.to_ir(
                 IRType.OPENQASM, gate_definitions=gate_definitions
             ).source,
             inputs=(
-                self._input_sets * self._euler_angles
-                if self._input_sets
-                else ParameterSets(self._euler_angles)
+                self._input_sets * euler_angles if self._input_sets else ParameterSets(euler_angles)
             ).as_dict(),
         )
+
+    def _get_euler_angles(self) -> dict[str, float] | None:
+        observables = self._observables
+        if not observables:
+            return None
+        return (
+            self._get_euler_angles_sum(observables)
+            if isinstance(observables, Sum)
+            else self._get_euler_angles_list(observables)
+        )
+
+    def _get_euler_angles_sum(self, observables: Sum) -> dict[str, float]:
+        euler_angles = defaultdict(list)
+        summands = observables.summands
+        if not observables.targets:
+            targets = self._circuit.qubits
+            for obs in summands:
+                for param, angle in obs.get_euler_angles(targets).items():
+                    euler_angles[param].append(angle)
+            return euler_angles
+        targets = {q for obs in summands for q in obs.targets}
+        for obs in summands:
+            obs_euler_angles = obs.euler_angles
+            for q in targets:
+                for param in euler_angle_parameter_names(q):
+                    euler_angles[param].append(obs_euler_angles.get(param, 0))
+        return euler_angles
+
+    def _get_euler_angles_list(self, observables: Sequence[Observable]) -> dict[str, float]:
+        euler_angles = defaultdict(list)
+        targets = {q for obs in observables for q in (obs.targets or self._circuit.qubits)}
+        for obs in observables:
+            if not obs.targets:
+                for param, angle in obs.get_euler_angles(targets).items():
+                    euler_angles[param].append(angle)
+            else:
+                obs_euler_angles = obs.euler_angles
+                for q in targets:
+                    for param in euler_angle_parameter_names(q):
+                        euler_angles[param].append(obs_euler_angles.get(param, 0))
+        return euler_angles
 
     def bind_observables_to_inputs(
         self,
@@ -228,11 +224,9 @@ class CircuitBinding:
                     "distribute your observable in advance using observable.summands.",
                     stacklevel=2,
                 )
-            euler_angles = {}
-            for target in {target for obs in observables for target in obs.targets}:
-                for param in euler_angle_parameter_names(target):
-                    euler_angles[param] = [obs.euler_angles.get(param, 0) for obs in observables]
-                if add_measure:
+            euler_angles = self._get_euler_angles()
+            if add_measure:
+                for target in {int(p.split("_")[-1]) for p in euler_angles}:
                     measure.measure(target)
             measure = Circuit().with_euler_angles(observables) + measure
             parameters = self._input_sets * euler_angles if parameters else euler_angles
