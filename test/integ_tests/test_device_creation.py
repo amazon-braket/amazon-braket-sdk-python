8d122d77bda504c3b451b4a332a6657858ddf71a
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2026-03-02 17:20:27 -0500

    fix: integ test fix after Aria-1 retirement

--- test/integ_tests/test_device_creation.py
+++ test/integ_tests/test_device_creation.py
@@ -17,7 +17,7 @@ from braket.aws import AwsDevice
 from braket.devices import Devices
 
 RIGETTI_ARN = "arn:aws:braket:us-west-1::device/qpu/rigetti/Ankaa-2"
-IONQ_ARN = "arn:aws:braket:us-east-1::device/qpu/ionq/Aria-1"
+IONQ_ARN = "arn:aws:braket:us-east-1::device/qpu/ionq/Forte-Enterprise-1"
 IQM_ARN = "arn:aws:braket:eu-north-1::device/qpu/iqm/Garnet"
 SIMULATOR_ARN = "arn:aws:braket:::device/quantum-simulator/amazon/sv1"
 PULSE_ARN = "arn:aws:braket:us-west-1::device/qpu/rigetti/Ankaa-2"
