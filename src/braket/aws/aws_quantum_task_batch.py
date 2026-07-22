866adbec20d4bc95957a6c8d0c1e0bc14b870e8d
Cody Wang <speller26@gmail.com>
2026-07-22 14:17:06 -0700

    fix: Format with new ruff rules

--- src/braket/aws/aws_quantum_task_batch.py
+++ src/braket/aws/aws_quantum_task_batch.py
@@ -107,7 +107,7 @@ class AwsQuantumTaskBatch(QuantumTaskBatch):
                 experimental capabilities. If `None`, the setting from the experimental
                 capability context will be used if active. Default: None.
             **aws_quantum_task_kwargs (Any): Arbitrary kwargs for `QuantumTask`.,
-        """  # noqa: E501
+        """  # ruff:ignore[line-too-long]
         self._tasks = AwsQuantumTaskBatch._execute(
             aws_session,
             device_arn,
